a874a8076b416ea4f12e90ee7e010e782dea646a
Sofia Marlop <romata1966@gmail.com>
2021-10-08 17:03:02 +0200

    Active checkbox only PHONG

--- ACG-Framework-STUDENT/ACG-Framework-master/src/Yourmaterial.cpp
+++ ACG-Framework-STUDENT/ACG-Framework-master/src/Yourmaterial.cpp
@@ -15,7 +15,11 @@ void yourmaterial::renderInMenu()
 	
 	ImGui::Combo("Output", (int*)&Application::instance->material_basic->material, "TEXTURE\0PHONG\0REFLECTIVE\0\REFRACTIVE\0");
 	ImGui::Combo("Texture", (int*)&Application::instance->material_basic->eTexture, "STONE\0GROUND\0");
-	ImGui::Checkbox("Show Texture", &Application::instance->material_basic->active_bool);
-	if (active_bool == 0)Application::instance->material_basic->u_active = 0.0f;
-	if (active_bool == 1)Application::instance->material_basic->u_active = 1.0f;
+	if (material==PHONG) 
+	{
+		ImGui::Checkbox("Show Texture", &Application::instance->material_basic->active_bool);
+		if (active_bool == 0)Application::instance->material_basic->u_active = 0.0f;
+		if (active_bool == 1)Application::instance->material_basic->u_active = 1.0f;
+	}
+	
 }
